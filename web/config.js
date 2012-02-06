524deba7cf8fe8b95deb84bd26942e403fbd78eb
Mike Primm <mike@primmhome.com>
2012-02-06 15:11:20 -0600

    Double-comment the comments in config.js - maybe stop them being uncommented?

--- web/config.js
+++ web/config.js
@@ -6,21 +6,21 @@ var config = {
 		sendmessage : 'up/sendmessage'
 	},
 
-	// For proxying webserver through php.
+	// // For proxying webserver through php.
 	// url: {
 	// configuration: 'up.php?path=configuration',
 	// update: 'up.php?path=world/{world}/{timestamp}',
 	// sendmessage: 'up.php?path=sendmessage'
 	// },
 
-	// For proxying webserver through aspx.
+	// // For proxying webserver through aspx.
 	// url: {
 	// configuration: 'up.aspx?path=configuration',
 	// update: 'up.aspx?path=world/{world}/{timestamp}',
 	// sendmessage: 'up.aspx?path=sendmessage'
 	// },
 
-	// For standalone (jsonfile) webserver.
+	// // For standalone (jsonfile) webserver.
 	// url: {
 	// configuration: 'standalone/dynmap_config.json?_={timestamp}',
 	// update: 'standalone/dynmap_{world}.json?_={timestamp}',
