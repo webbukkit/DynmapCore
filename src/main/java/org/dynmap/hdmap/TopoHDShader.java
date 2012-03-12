f392d44b990983e7e91e80dbb3b14a53bab245ea
Mike Primm <mike@primmhome.com>
2012-03-11 22:47:28 -0500

    Add 'hiddenids' option to control hiding of blocks in topo renderer: also add topo-noplants sample to hide all trees, plants, and the like

--- src/main/java/org/dynmap/hdmap/TopoHDShader.java
+++ src/main/java/org/dynmap/hdmap/TopoHDShader.java
@@ -2,6 +2,8 @@ package org.dynmap.hdmap;
 
 import static org.dynmap.JSONUtils.s;
 
+import java.util.List;
+
 import org.dynmap.Color;
 import org.dynmap.ConfigurationNode;
 import org.dynmap.DynmapCore;
@@ -16,6 +18,7 @@ public class TopoHDShader implements HDShader {
     private Color linecolor;  /* Color for topo lines */
     private Color fillcolor[];  /* Color for nontopo surfaces */
     private Color watercolor;
+    private int[] hiddenids;
     
     private Color readColor(String id, ConfigurationNode cfg) {
         String lclr = cfg.getString(id, null);
@@ -32,9 +35,9 @@ public class TopoHDShader implements HDShader {
     public TopoHDShader(DynmapCore core, ConfigurationNode configuration) {
         name = (String) configuration.get("name");
         
-        fillcolor = new Color[128];   /* Color by Y */
+        fillcolor = new Color[256];   /* Color by Y */
         /* Load defined colors from parameters */
-        for(int i = 0; i < 128; i++) {
+        for(int i = 0; i < 256; i++) {
             fillcolor[i] = readColor("color" + i,  configuration);
         }
         linecolor = readColor("linecolor",  configuration);
@@ -43,11 +46,11 @@ public class TopoHDShader implements HDShader {
         if(fillcolor[0] == null) {
             fillcolor[0] = new Color(0, 0, 0);
         }
-        if(fillcolor[127] == null) {
-            fillcolor[127] = new Color(255, 255, 255);
+        if(fillcolor[255] == null) {
+            fillcolor[255] = new Color(255, 255, 255);
         }
         int starty = 0;
-        for(int i = 1; i < 128; i++) {
+        for(int i = 1; i < 256; i++) {
             if(fillcolor[i] != null) {  /* Found color? */
                 int delta = i - starty;
                 Color c0 = fillcolor[starty];
@@ -57,6 +60,19 @@ public class TopoHDShader implements HDShader {
                     fillcolor[starty + j] = new Color((c0.getRed()*(delta-j) + c1.getRed()*j)/delta, (c0.getGreen()*(delta-j) + c1.getGreen()*j)/delta, (c0.getBlue()*(delta-j) + c1.getBlue()*j)/delta);
                 }
                 starty = i;  /* New start color */
+            }
+        }
+        hiddenids = new int[2048];
+        hiddenids[0] = 0x1; /* Air is hidden always */
+        List<Object> hidden = configuration.getList("hiddenids");
+        if(hidden != null) {
+            for(Object o : hidden) {
+                if(o instanceof Integer) {
+                    int v = ((Integer)o);
+                    if((v > 0) && (v < 65535)) {
+                        hiddenids[v >> 5] |= (1 << (v & 0x1F));
+                    }
+                }
             }
         }
     }
@@ -123,7 +139,7 @@ public class TopoHDShader implements HDShader {
             /* Compute divider for Y - to map to existing color range */
             int wh = mapiter.getWorldHeight();
             heightshift = 0;
-            while(wh > 128) {
+            while(wh > 256) {
                 heightshift++;
                 wh >>= 1;
             }
@@ -157,6 +173,12 @@ public class TopoHDShader implements HDShader {
                 color[i].setTransparent();
         }
         
+        private final boolean isHidden(int id) {
+            if((id == 0) || ((hiddenids[id >> 5] & (1 << (id & 0x1F))) != 0)) {
+                return true;
+            }
+            return false;
+        }
         /**
          * Process next ray step - called for each block on route
          * @return true if ray is done, false if ray needs to continue
@@ -164,7 +186,7 @@ public class TopoHDShader implements HDShader {
         public boolean processBlock(HDPerspectiveState ps) {
             int blocktype = ps.getBlockTypeID();
             
-            if(blocktype == 0) {
+            if(isHidden(blocktype)) {
                 return false;
             }
             /* See if we're close to an edge */
@@ -174,10 +196,10 @@ public class TopoHDShader implements HDShader {
             case Y_MINUS:
             case Y_PLUS:
                 if((linecolor != null) &&
-                        (((xyz[0] == 0) && (mapiter.getBlockTypeIDAt(BlockStep.X_MINUS) == 0)) ||
-                        ((xyz[0] == (scale-1)) && (mapiter.getBlockTypeIDAt(BlockStep.X_PLUS) == 0)) ||
-                        ((xyz[2] == 0) && (mapiter.getBlockTypeIDAt(BlockStep.Z_MINUS) == 0)) ||
-                        ((xyz[2] == (scale-1)) && (mapiter.getBlockTypeIDAt(BlockStep.Z_PLUS) == 0)))) {
+                        (((xyz[0] == 0) && (isHidden(mapiter.getBlockTypeIDAt(BlockStep.X_MINUS)))) ||
+                        ((xyz[0] == (scale-1)) && (isHidden(mapiter.getBlockTypeIDAt(BlockStep.X_PLUS)))) ||
+                        ((xyz[2] == 0) && (isHidden(mapiter.getBlockTypeIDAt(BlockStep.Z_MINUS)))) ||
+                        ((xyz[2] == (scale-1)) && (isHidden(mapiter.getBlockTypeIDAt(BlockStep.Z_PLUS)))))) {
                     c.setColor(linecolor);
                 }
                 else if((watercolor != null) && ((blocktype == 8) || (blocktype == 9))) {
