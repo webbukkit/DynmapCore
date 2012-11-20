cb96b19e78f4a11e6d036175e7b0eaa922343acf
Mike Primm <mike@primmhome.com>
2012-11-20 00:07:02 -0600

    Switch stairs to custom renderer - support corner stairs

---
+++ src/main/java/org/dynmap/hdmap/renderer/StairBlockRenderer.java
@@ -0,0 +1,214 @@
+package org.dynmap.hdmap.renderer;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+
+import org.dynmap.renderer.CustomRenderer;
+import org.dynmap.renderer.MapDataContext;
+import org.dynmap.renderer.RenderPatch;
+import org.dynmap.renderer.RenderPatchFactory;
+import org.dynmap.renderer.RenderPatchFactory.SideVisible;
+import org.dynmap.utils.BlockStep;
+import org.dynmap.utils.MapIterator;
+
+public class StairBlockRenderer extends CustomRenderer {
+    private static final int TEXTURE_SIDES = 0;
+    private static final int TEXTURE_TOP = 1;
+    private static final int TEXTURE_BOTTOM = 2;
+    private int blkid;    
+    private static BitSet stair_ids = new BitSet();
+        
+    // Array of meshes for normal steps - index = (data value & 7)
+    private RenderPatch[][] stepmeshes = new RenderPatch[8][];
+    // Array of meshes for 3/4 steps - index = (data value & 7), with extra one clockwise from normal step
+    private RenderPatch[][] step_3_4_meshes = new RenderPatch[8][];
+    // Array of meshes for 1/4 steps - index = (data value & 7), with clockwise quarter clopped from normal step
+    private RenderPatch[][] step_1_4_meshes = new RenderPatch[8][];
+    
+    @Override
+    public boolean initializeRenderer(RenderPatchFactory rpf, int blkid, int blockdatamask, String custparm) {
+        if(!super.initializeRenderer(rpf, blkid, blockdatamask, custparm))
+            return false;
+        this.blkid = blkid; /* Remember our block ID */
+        stair_ids.set(blkid);   /* Mark block as a stair */
+        /* Build step meshes */
+        for(int i = 0; i < 8; i++) {
+            stepmeshes[i] = buildStepMeshes(rpf, i);   
+            step_1_4_meshes[i] = buildCornerStepMeshes(rpf, i);   
+            step_3_4_meshes[i] = buildIntCornerStepMeshes(rpf, i);   
+        }
+        
+        return true;
+    }
+
+    @Override
+    public int getMaximumTextureCount() {
+        return 2;
+    }
+
+    private RenderPatch[] buildStepMeshes(RenderPatchFactory rpf, int dat) {
+        ArrayList<RenderPatch> list = new ArrayList<RenderPatch>();
+        /* If inverted, add half top */
+        if((dat & 0x4) != 0) {
+            addBox(rpf, list, 0, 1, 0.5, 1, 0, 1);
+        }
+        else {  // Else, add half bottom
+            addBox(rpf, list, 0, 1, 0.0, 0.5, 0, 1);
+        }
+        switch(dat & 0x3) {
+            case 0: 
+                addBox(rpf, list, 0.5, 1, 0, 1, 0, 1);
+                break;
+            case 1:
+                addBox(rpf, list, 0, 0.5, 0, 1, 0, 1);
+                break;
+            case 2:
+                addBox(rpf, list, 0, 1, 0, 1, 0.5, 1);
+                break;
+            case 3:
+                addBox(rpf, list, 0, 1, 0, 1, 0, 0.5);
+                break;
+        }
+        return list.toArray(new RenderPatch[list.size()]);
+    }
+
+    private RenderPatch[] buildCornerStepMeshes(RenderPatchFactory rpf, int dat) {
+        ArrayList<RenderPatch> list = new ArrayList<RenderPatch>();
+        /* If inverted, add half top */
+        if((dat & 0x4) != 0) {
+            addBox(rpf, list, 0, 1, 0.5, 1, 0, 1);
+        }
+        else {  // Else, add half bottom
+            addBox(rpf, list, 0, 1, 0.0, 0.5, 0, 1);
+        }
+        switch(dat & 0x3) {
+            case 0: 
+                addBox(rpf, list, 0.5, 1, 0, 1, 0, 0.5);
+                break;
+            case 1:
+                addBox(rpf, list, 0, 0.5, 0, 1, 0, 0.5);
+                break;
+            case 2:
+                addBox(rpf, list, 0, 0.5, 0, 1, 0.5, 1);
+                break;
+            case 3:
+                addBox(rpf, list, 0.5, 1, 0, 1, 0.5, 1);
+                break;
+        }
+        return list.toArray(new RenderPatch[list.size()]);
+    }
+
+    private RenderPatch[] buildIntCornerStepMeshes(RenderPatchFactory rpf, int dat) {
+        ArrayList<RenderPatch> list = new ArrayList<RenderPatch>();
+        /* If inverted, add half top */
+        if((dat & 0x4) != 0) {
+            addBox(rpf, list, 0, 1, 0.5, 1, 0, 1);
+        }
+        else {  // Else, add half bottom
+            addBox(rpf, list, 0, 1, 0.0, 0.5, 0, 1);
+        }
+        switch(dat & 0x3) {
+            case 0: 
+                addBox(rpf, list, 0.5, 1, 0, 1, 0, 1);
+                addBox(rpf, list, 0, 0.5, 0, 1, 0, 0.5);
+                break;
+            case 1:
+                addBox(rpf, list, 0.5, 1, 0, 1, 0, 1);
+                addBox(rpf, list, 0, 0.5, 0, 1, 0.5, 1);
+                break;
+            case 2:
+                addBox(rpf, list, 0, 0.5, 0, 1, 0, 1);
+                addBox(rpf, list, 0.5, 1, 0, 1, 0, 0.5);
+                break;
+            case 3:
+                addBox(rpf, list, 0, 0.5, 0, 1, 0, 1);
+                addBox(rpf, list, 0.5, 1, 0, 1, 0.5, 1);
+                break;
+        }
+        return list.toArray(new RenderPatch[list.size()]);
+    }
+
+    private void addBox(RenderPatchFactory rpf, List<RenderPatch> list, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax)  {
+        /* Add top */
+        list.add(rpf.getPatch(0, ymax, 1, 1, ymax, 1, 0, ymax, 0, xmin, xmax, 1-zmax, 1-zmin, SideVisible.TOP, TEXTURE_TOP));
+        /* Add bottom */
+        list.add(rpf.getPatch(0, ymin, 1, 1, ymin, 1, 0, ymin, 0, xmin, xmax, 1-zmax, 1-zmin, SideVisible.TOP, TEXTURE_BOTTOM));
+        /* Add minX side */
+        list.add(rpf.getPatch(xmin, 0, 0, xmin, 0, 1, xmin, 1, 0, zmin, zmax, ymin, ymax, SideVisible.TOP, TEXTURE_SIDES));
+        /* Add maxX side */
+        list.add(rpf.getPatch(xmax, 0, 1, xmax, 0, 0, xmax, 1, 1, 1-zmax, 1-zmin, ymin, ymax, SideVisible.TOP, TEXTURE_SIDES));
+        /* Add minZ side */
+        list.add(rpf.getPatch(1, 0, zmin, 0, 0, zmin, 1, 1, zmin, 1-xmax, 1-xmin, ymin, ymax, SideVisible.TOP, TEXTURE_SIDES));
+        /* Add maxZ side */
+        list.add(rpf.getPatch(0, 0, zmax, 1, 0, zmax, 0, 1, zmax, xmin, xmax, ymin, ymax, SideVisible.TOP, TEXTURE_SIDES));
+    }
+    //  Steps
+    // 0 = up to east
+    // 1 = up to west
+    // 2 = up to south
+    // 3 = up to north
+    //  Corners
+    // 0 = NE
+    // 1 = NW
+    // 2 = SW
+    // 3 = SE
+    //  Interior Corners
+    // 0 = open to SW
+    // 1 = open to NW
+    // 2 = open to SE
+    // 3 = open to NE
+    private static final int off_x[] = { 1, -1, 0, 0, 1, -1, 0, 0 };
+    private static final int off_z[] = { 0, 0, 1, -1, 0, 0, 1, -1 };
+    private static final int match1[] = { 2, 3, 0, 1, 6, 7, 4, 5 };
+    private static final int corner1[] = { 3, 1, 3, 1, 7, 5, 7, 5 };
+    private static final int icorner1[] = { 1, 2, 1, 2, 5, 6, 5, 6 };
+    private static final int match2[] = { 3, 2, 1, 0, 7, 6, 5, 4 };
+    private static final int corner2[] = { 0, 2, 2, 0, 4, 6, 6, 4 };
+    private static final int icorner2[] = { 0, 3, 3, 0, 4, 7, 7, 4 };
+    
+    @Override
+    public RenderPatch[] getRenderPatchList(MapDataContext ctx) {
+        int data = ctx.getBlockData() & 0x07;   /* Get block data */
+        /* Check block behind stair */
+        int cornerid = ctx.getBlockTypeIDAt(off_x[data], 0, off_z[data]);
+        if(stair_ids.get(cornerid)) {   /* If it is a stair */
+            int cornerdat = ctx.getBlockDataAt(off_x[data], 0, off_z[data]);
+            if(cornerdat == match1[data]) {    /* If right orientation */
+                /* Make sure we don't have matching stair to side */
+                int sideid = ctx.getBlockTypeIDAt(-off_x[cornerdat], 0, -off_z[cornerdat]);
+                if((!stair_ids.get(sideid)) || (ctx.getBlockDataAt(-off_x[cornerdat], 0, -off_z[cornerdat]) != data)) {
+                    return step_1_4_meshes[corner1[data]];
+                }
+            }
+            else if(cornerdat == match2[data]) {   /* If other orientation */
+                /* Make sure we don't have matching stair to side */
+                int sideid = ctx.getBlockTypeIDAt(-off_x[cornerdat], 0, -off_z[cornerdat]);
+                if((!stair_ids.get(sideid)) || (ctx.getBlockDataAt(-off_x[cornerdat], 0, -off_z[cornerdat]) != data)) {
+                    return step_1_4_meshes[corner2[data]];
+                }
+            }
+        }
+        /* Check block in front of stair */
+        cornerid = ctx.getBlockTypeIDAt(-off_x[data], 0, -off_z[data]);
+        if(stair_ids.get(cornerid)) {   /* If it is a stair */
+            int cornerdat = ctx.getBlockDataAt(-off_x[data], 0, -off_z[data]);
+            if(cornerdat == match1[data]) {    /* If right orientation */
+                /* Make sure we don't have matching stair to side */
+                int sideid = ctx.getBlockTypeIDAt(off_x[cornerdat], 0, off_z[cornerdat]);
+                if((!stair_ids.get(sideid)) || (ctx.getBlockDataAt(off_x[cornerdat], 0, off_z[cornerdat]) != data)) {
+                    return step_3_4_meshes[icorner1[data]];
+                }
+            }
+            else if(cornerdat == match2[data]) {   /* If other orientation */
+                /* Make sure we don't have matching stair to side */
+                int sideid = ctx.getBlockTypeIDAt(off_x[cornerdat], 0, off_z[cornerdat]);
+                if((!stair_ids.get(sideid)) || (ctx.getBlockDataAt(off_x[cornerdat], 0, off_z[cornerdat]) != data)) {
+                    return step_3_4_meshes[icorner2[data]];
+                }
+            }
+        }
+        
+        return stepmeshes[data];
+    }    
+}
