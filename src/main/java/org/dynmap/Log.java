5af507945753bd9b094a733b7ae4548174fe64d5
Mike Primm <mike@primmhome.com>
2013-03-29 21:45:17 -0500

    Add support for injecting logger to core

--- src/main/java/org/dynmap/Log.java
+++ src/main/java/org/dynmap/Log.java
@@ -4,29 +4,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Log {
-    protected static final Logger log = Logger.getLogger("Minecraft");
-    protected static final String LOG_PREFIX = "[dynmap] ";
+    protected static Logger log = Logger.getLogger("Minecraft");
+    protected static String prefix = "[dynmap] ";
     public static boolean verbose = false;
+    public static void setLogger(Logger logger, String pre) {
+        log = logger;
+        if((pre != null) && (pre.length() > 0))
+            prefix = pre + " ";
+        else
+            prefix = "";
+    }
     public static void info(String msg) {
-        log.log(Level.INFO, LOG_PREFIX + msg);
+        log.log(Level.INFO, prefix + msg);
     }
     public static void verboseinfo(String msg) {
         if(verbose)
-            log.log(Level.INFO, LOG_PREFIX + msg);
+            log.log(Level.INFO, prefix + msg);
     }
     public static void severe(Throwable e) {
-        log.log(Level.SEVERE, LOG_PREFIX + "Exception occured: ", e);
+        log.log(Level.SEVERE, prefix + "Exception occured: ", e);
     }
     public static void severe(String msg) {
-        log.log(Level.SEVERE, LOG_PREFIX + msg);
+        log.log(Level.SEVERE, prefix + msg);
     }
     public static void severe(String msg, Throwable e) {
-        log.log(Level.SEVERE, LOG_PREFIX + msg, e);
+        log.log(Level.SEVERE, prefix + msg, e);
     }
     public static void warning(String msg) {
-        log.log(Level.WARNING, LOG_PREFIX + msg);
+        log.log(Level.WARNING, prefix + msg);
     }
     public static void warning(String msg, Throwable e) {
-        log.log(Level.WARNING, LOG_PREFIX + msg, e);
+        log.log(Level.WARNING, prefix + msg, e);
     }
 }
